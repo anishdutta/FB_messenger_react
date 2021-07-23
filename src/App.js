8b58fb5e18d4810f56267d069075e09acc80b3f0
Anish Dutta <60481056+anishdutta@users.noreply.github.com>
2021-07-23 13:23:00 +0530

    navbar fix

--- src/App.js
+++ src/App.js
@@ -17,17 +17,32 @@ function App() {
     <div className="container-fluid">
           <Router>
       <div>
-        <nav>
-          <ul>
-           
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/users">Posts</Link>
-            </li>
-          </ul>
-        </nav>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <div className="container-fluid">
+    
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+        <Link className="nav-link" to="/">Home</Link>
+        </li>
+        <li className="nav-item">
+        <Link  className="nav-link" to="/users">Posts</Link>
+        </li>
+        
+        
+        
+      </ul>
+      <div class="fb-btn d-flex">
+        <Login></Login>
+        </div>
+     
+    </div>
+  </div>
+</nav>
+      
 
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
@@ -36,7 +51,7 @@ function App() {
             <posts />
           </Route>
           <Route path="/">
-            <Login></Login>
+           
             <Inboxlist></Inboxlist>
           </Route>
         </Switch>
